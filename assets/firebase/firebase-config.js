6151020c25f0063c27b446a6c6398a26e68bb071
rakhaafd <rakhafausta07@gmail.com>
2025-08-26 12:53:52 +0700

    📌 chore(auth): added login feature with firebase

---
+++ assets/firebase/firebase-config.js
@@ -0,0 +1,12 @@
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBMkFP0GhEsGxhZNJ3bOK9MpFZPz8DjnQI",
+  authDomain: "cineview-ee47d.firebaseapp.com",
+  projectId: "cineview-ee47d",
+  storageBucket: "cineview-ee47d.firebasestorage.app",
+  messagingSenderId: "994927892314",
+  appId: "1:994927892314:web:e23ff991dc5b670733c563"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
